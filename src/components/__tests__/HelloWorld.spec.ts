140a13efbbceaebda94035935b00fb731fe89b1e
Adryan <adrianrogowski80@gmail.com>
2023-04-21 00:30:19 +0200

    Fix Test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,12 +1,21 @@
 import { describe, it, expect } from 'vitest'
+import { Quasar } from 'quasar'
 
 import { mount } from '@vue/test-utils'
 import LoginView from '../../views/LoginView.vue'
 
-describe('LoginPage', () => {
-  it('renders properly', () => {
-    const wrapper = mount(LoginView)
-    wrapper.find('[aria-label="Email"]').setValue('email1@example.com')
-    expect(true)
+const wrapperFactory = () =>
+  mount(LoginView, {
+    global: {
+      plugins: [Quasar]
+    }
+  })
+
+describe('MyForm', () => {
+  it('should fill in email input', async () => {
+    expect(LoginView).toBeTruthy()
+    const wrapper = wrapperFactory()
+
+    expect(wrapper.find('h2').exists()).toBeTruthy()
   })
 })
