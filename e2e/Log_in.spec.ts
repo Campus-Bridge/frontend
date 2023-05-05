9bf72c6b2c4703989b1fc774f7792e914aa37ee7
Neliyenn <aleksandraj.poczta@gmail.com>
2023-05-06 01:34:23 +0200

    tests correction and navigation bar test

--- e2e/Log_in.spec.ts
+++ e2e/Log_in.spec.ts
@@ -1,21 +1,16 @@
 import { test, expect } from '@playwright/test'
 
-const { firefox } = require('playwright');
+test('Log in', async ({page}) => {
 
-test('Log in', async () => {
-
-  const browser = await firefox.launch()
-  const page = await browser.newPage()
   await page.goto('/')
 
   await page.fill('input[type="email"]', 'email1@example.com')
   await page.fill('input[type="password"]', 'password1')
 
   await page.check('text=Remember me')
-
+  
   await page.click('button[type="submit"]')
-  await page.waitForNavigation();
+  await page.waitForTimeout(3000)
 
-  await expect(page.url()).toContain('/dashboard')
-
+  expect(page.url()).toContain('dashboard')
 })
