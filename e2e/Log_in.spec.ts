99222316f2ddf5d6d6a75abdaca9aae4517a2da4
Neliyenn <aleksandraj.poczta@gmail.com>
2023-05-06 21:26:31 +0200

    log in using incorrect credentials test

--- e2e/Log_in.spec.ts
+++ e2e/Log_in.spec.ts
@@ -1,16 +1,31 @@
 import { test, expect } from '@playwright/test'
 
-test('Log in', async ({page}) => {
+test.afterEach(async ({page}) => {
+
+  await page.close()
+});
+test('Log in using correct credentials', async ({page}) => {
 
   await page.goto('/')
 
   await page.fill('input[type="email"]', 'email1@example.com')
   await page.fill('input[type="password"]', 'password1')
-
-  await page.check('text=Remember me')
   
   await page.click('button[type="submit"]')
   await page.waitForTimeout(3000)
 
-  expect(page.url()).toContain('dashboard')
+  expect(page.url()).toContain('/dashboard')
+})
+
+test('Log in using incorrect credentials', async ({page}) => {
+
+  await page.goto('/')
+
+  await page.fill('input[type="email"]', 'email1321@example.com')
+  await page.fill('input[type="password"]', 'passwordD421')
+  
+  await page.click('button[type="submit"]')
+  await page.waitForTimeout(3000)
+
+  expect(page.url()).toBe('http://localhost:5173/')
 })
